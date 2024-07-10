f018ca3510a02d457a5e0d4da0a87f38e6661c85
Tim Gerry Geerts <timgerry.geerts@infosupport.com>
2024-07-10 11:06:19 +0200

    Unit testing

--- src/app/pipes/optional.pipe.spec.ts
+++ src/app/pipes/optional.pipe.spec.ts
@@ -1,8 +1,29 @@
 import { OptionalPipe } from './optional.pipe';
 
 describe('OptionalPipe', () => {
-  it('create an instance', () => {
-    const pipe = new OptionalPipe();
-    expect(pipe).toBeTruthy();
+
+  let sut: OptionalPipe;
+
+  beforeEach(() => {
+    sut = new OptionalPipe();
+  });
+
+  it('should create an instance', () => {
+    expect(sut).toBeTruthy();
+  });
+
+  it('should display present values', () => {
+    const actual = sut.transform('hello');
+    expect(actual).toBe('hello');
+  })
+
+  it('should replace value', () => {
+    const actual = sut.transform(undefined);
+    expect(actual).toBe('-');
+  });
+
+  it('should replace value with specified string', () => {
+    const actual = sut.transform(undefined, '/');
+    expect(actual).toBe('/');
   });
 });

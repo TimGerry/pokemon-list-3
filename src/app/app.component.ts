96efe4eafd74cfc2f45550709d2fbd5179067d52
Tim Gerry Geerts <timgerry.geerts@infosupport.com>
2024-07-10 09:46:42 +0200

    Getting started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pokemon } from '../models/pokemon.model';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pokémon List';
+  pokemonList: Pokemon[] = [
+    { name: 'garchomp', type: 'ground', type2: 'dragon', attack: 'outrage', level: 60 },
+    {name: 'squirtle', type: 'water', attack: 'water gun', level: 5 },
+    { name: 'magikarp', type: 'water', attack: 'splash', level: 100 }
+  ]
+
+  attack(pokemon: Pokemon) {
+    window.alert(`${pokemon.name} used ${pokemon.attack}`);
+  }
 }
